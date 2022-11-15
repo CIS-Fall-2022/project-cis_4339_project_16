24c6fac5ab129473aa0e9cf3053cbe871039028f
cnguy430 <61709386+cnguy430@users.noreply.github.com>
2022-11-15 00:02:29 -0600

    validation error display

--- frontend/src/components/updateClient.vue
+++ frontend/src/components/updateClient.vue
@@ -194,6 +194,19 @@ export default {
   />
 </FormulateForm>
 
+// ************** referred to https://vuejsdevelopers.com/2018/08/27/vue-js-form-handling-vuelidate/
+
+<div class="form-group" :class="{ 'hasError': v.$error }">
+  <label class="mr-2 font-bold text-grey">Email</label>
+  <input type="email" class="input" v-model="email" placeholder="user@yahoo.com" @input="v.$touch()">
+  <div class="text-sm mt-2 text-red" v-if="v.$error">
+    <div v-if="!v.required">Email is required</div>
+    <div v-if="!v.notGmail">Email should not be a Gmail one</div>
+    <div v-if="!v.isEmailAvailable">Email is not available (less than 10 char)</div>
+    <div v-if="!v.email">Email is not a properly formatted email address</div>
+  </div>
+</div>
+
 };
 </script>
 <template>
